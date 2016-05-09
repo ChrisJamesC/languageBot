e1b33c245d993d989a9a7a43af389871805a5915
Christopher Chiche <christopher.chiche@gmail.com>
2016-05-09 23:24:21 +0200

    add messages

--- possibleAnswers.js
+++ possibleAnswers.js
@@ -23,8 +23,12 @@ module.exports = {
   FUSSBALL: textMessage('"Fußball" is "Football" in English'), 
   FEEDBACK_Q: buttonMessage("Do you want to receive advanced feedback for 1CHF a month?\n",[{t:"Yes",p:"subscriptionYes"},{t:"No","p":"none"}]),  
   FEEDBACK_R: textMessage("Here are your major areas of improvement\nI do well -> I am doing well\nRepartition per mistakes\n  Vocabulary: 50%\n  Grammar: 30%\n  Puncuation: 10%"),
-  CONV_SUBJECT: buttonMessage("Cool. What do you want to talk about today?" ,[{t:"Sports", p: "sports"}, {t:"News", p:"none"}, {t:"Famous People", p:"none"}]),
+  CONV_SUBJECT: buttonMessage("Cool. What do you want to talk about today?" ,[{t:"Sports", p: "sports"}, {t:"News", p:"news"}, {t:"Famous People", p:"famousPeople"}]),
   SPORT_Q: textMessage("What’s your favorite sport?"), 
+  NEWS_Q: textMessage("What do you think about Donald Trump?"),
+  FAMOUS_PEOPLE_Q: textMessage("What's Kim Kardashian the best at?"),
   DUMB: textMessage("Sorry I didn't understand"), 
-  BYE: textMessage("Thanks a lot! Please visit www.lingobot.co for more information.")
+  BYE: textMessage("Thanks a lot! Please visit www.lingobot.co for more information."),
+  WHY_NOT: textMessage("Why not?"),
+  WE_AGREE: textMessage("That's what I thought!")
 }
