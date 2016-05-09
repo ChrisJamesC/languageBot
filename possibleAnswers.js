b2856c2a8fa451fd82e9c341a343b3966a900566
Christopher Chiche <christopher.chiche@gmail.com>
2016-05-09 23:37:48 +0200

    handle no subscription

--- possibleAnswers.js
+++ possibleAnswers.js
@@ -21,7 +21,7 @@ module.exports = {
   GREETINGS: textMessage("How are you doing?"), 
   DO_WELL:  buttonMessage('Did you mean "doing well"?', [{t:"Yes", p: "correctionOK"}, {t:"No", p:"correctionBad"}]),
   FUSSBALL: textMessage('"Fußball" is "Football" in English'), 
-  FEEDBACK_Q: buttonMessage("Do you want to receive advanced feedback for 1CHF a month?\n",[{t:"Yes",p:"subscriptionYes"},{t:"No","p":"none"}]),  
+  FEEDBACK_Q: buttonMessage("Do you want to receive advanced feedback for 1CHF a month?\n",[{t:"Yes",p:"subscriptionYes"},{t:"No","p":"subscriptionNo"}]),  
   FEEDBACK_R: textMessage("Here are your major areas of improvement\nI do well -> I am doing well\nRepartition per mistakes\n  Vocabulary: 50%\n  Grammar: 30%\n  Puncuation: 10%"),
   CONV_SUBJECT: buttonMessage("Cool. What do you want to talk about today?" ,[{t:"Sports", p: "sports"}, {t:"News", p:"news"}, {t:"Famous People", p:"famousPeople"}]),
   SPORT_Q: textMessage("What’s your favorite sport?"), 
