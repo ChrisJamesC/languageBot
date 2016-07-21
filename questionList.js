062ec540513bfd873a3839196eb3ccbbdfd2305f
Christopher Chiche <christopher.chiche@gmail.com>
2016-07-21 20:36:11 +0200

    :japan:

--- questionList.js
+++ questionList.js
@@ -6,9 +6,50 @@ questions = [
     },
     {
         question: "_______ cat is absolutely beautiful.",
-        answers: ["They’re ", "There", "Their"],
+        answers: ["They’re", "There", "Their"],
         correct: 2
     },
+    {
+        question: "______ shoes that I’m wearing right now are so comfortable.",
+        answers: ["This", "These", "Those"],
+        correct: 1
+    },
+    {
+        question: "The dog is chasing _____ tail.",
+        answers: ["it’s", "its", "that’s"],
+        correct: 1
+    },
+    {
+        question: "She has five _______.",
+        answers: ["child", "childrens", "children"],
+        correct: 2
+    },
+    {
+        question: "The ____________ are in the field.",
+        answers: ["horse’s", "horse is", "horses"],
+        correct: 2
+    },
+    {
+        question: "________ hair looks really nice.",
+        answers: ["You’re", "Your", "Yours"],
+        correct: 1
+    },
+    {
+        question: "Mary has ______ brothers.",
+        answers: ["to", "too", "two"],
+        correct: 2
+    },
+    {
+        question: "She is better in sports _____ him.",
+        answers: ["then", "than", "that"],
+        correct: 1
+    },
+    {
+        question: "_______ did you invite to your birthday party?",
+        answers: ["Who", "Whom", "Whose"],
+        correct: 0
+    }
+
 ]
 module.exports = ({
     questions
